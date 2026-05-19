b02595ee2ac91eabe887a5ec33173e5bacbda862
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2026-05-19 14:16:30 +0200

    Add basic grid structure

---
+++ src/grid.rs
@@ -0,0 +1,25 @@
+﻿use std::collections::HashMap;
+use crate::coords::{Point2D, Vector2D};
+
+type GridPoint = Point2D<i32>;
+type GridVector = Vector2D<i32>;
+
+struct ChunkBounds {
+    origin: GridPoint,
+    width: i32,
+    height: i32,
+}
+
+struct Chunk<T> {
+    position: ChunkBounds,
+    cells: Vec<T>,
+}
+
+trait Chunker {
+    fn resolve_chunk_origin(&self, _:&GridPoint) -> GridPoint;
+    fn resolve_chunk_bounds(&self, _:&GridPoint) -> ChunkBounds;
+}
+
+struct Grid<T> {
+    chunks: HashMap<GridPoint, Chunk<T>>,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 ﻿mod coords;
+mod grid;
