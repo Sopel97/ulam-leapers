1983c9b683a6cae5b0576c9300d0f03b17299681
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2026-05-21 00:37:05 +0200

    Improve AVX-512 implementation for bit_transform

--- src/algo/bit_transpose/avx512bw.rs
+++ src/algo/bit_transpose/avx512bw.rs
@@ -11,33 +11,38 @@ unsafe fn bit_transpose_avx512bw_impl(input: &[u8], output: &mut [u8]) {
 
     let chunks = input.len() / 64;
 
+    let m0 = _mm512_set1_epi8(0x01);
+    let m1 = _mm512_set1_epi8(0x02);
+    let m2 = _mm512_set1_epi8(0x04);
+    let m3 = _mm512_set1_epi8(0x08);
+    let m4 = _mm512_set1_epi8(0x10);
+    let m5 = _mm512_set1_epi8(0x20);
+    let m6 = _mm512_set1_epi8(0x40);
+    let m7 = _mm512_set1_epi8(-128i8);
+
     for _ in 0..chunks {
         unsafe {
-            let v0 = _mm512_loadu_si512(in_ptr as *const __m512i);
-            
+            let v = _mm512_loadu_si512(in_ptr as *const __m512i);
+
             macro_rules! process_bit {
-                ($v0:ident, $bit_index:literal, $out_ptr:ident) => {
-                    // move target bit into sign bit
-                    let shifted0 = _mm512_slli_epi16($v0, 7 - $bit_index);
-            
-                    // collect sign bits
-                    let mask0 = _mm512_movepi8_mask(shifted0) as u64;
-            
-                    // store 16 gathered bits
-                    *($out_ptr as *mut u64) = mask0;
-            
+                ($v:ident, $bitmask:ident, $out_ptr:ident) => {
+                    // Test specific bit directly into the mask register.
+                    let k = _mm512_test_epi8_mask($v, $bitmask);
+
+                    _store_mask64($out_ptr as *mut __mmask64, k);
+
                     $out_ptr = $out_ptr.add(8);
                 };
             }
-            
-            process_bit!(v0, 0, out_ptr);
-            process_bit!(v0, 1, out_ptr);
-            process_bit!(v0, 2, out_ptr);
-            process_bit!(v0, 3, out_ptr);
-            process_bit!(v0, 4, out_ptr);
-            process_bit!(v0, 5, out_ptr);
-            process_bit!(v0, 6, out_ptr);
-            process_bit!(v0, 7, out_ptr);
+
+            process_bit!(v, m0, out_ptr);
+            process_bit!(v, m1, out_ptr);
+            process_bit!(v, m2, out_ptr);
+            process_bit!(v, m3, out_ptr);
+            process_bit!(v, m4, out_ptr);
+            process_bit!(v, m5, out_ptr);
+            process_bit!(v, m6, out_ptr);
+            process_bit!(v, m7, out_ptr);
 
             in_ptr = in_ptr.add(64);
         }
