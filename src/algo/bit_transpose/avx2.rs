32cc0977e30e02875b92b6f0d65a3f0f7a5ee2b4
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2026-05-21 00:26:15 +0200

    In bit_transpose use macro definitions closer to where they are used.

--- src/algo/bit_transpose/avx2.rs
+++ src/algo/bit_transpose/avx2.rs
@@ -1,22 +1,4 @@
-﻿macro_rules! process_bit {
-    ($v0:ident, $v1:ident, $bit_index:literal, $out_ptr:ident) => {
-        // move target bit into sign bit
-        let shifted0 = _mm256_slli_epi16($v0, 7 - $bit_index);
-        let shifted1 = _mm256_slli_epi16($v1, 7 - $bit_index);
-
-        // collect sign bits
-        let mask0 = _mm256_movemask_epi8(shifted0) as u32;
-        let mask1 = _mm256_movemask_epi8(shifted1) as u32;
-
-        // store 16 gathered bits
-        *($out_ptr as *mut u32) = mask0;
-        *($out_ptr.add(4) as *mut u32) = mask1;
-
-        $out_ptr = $out_ptr.add(8);
-    };
-}
-
-#[target_feature(enable = "avx2")]
+﻿#[target_feature(enable = "avx2")]
 unsafe fn bit_transpose_avx2_impl(input: &[u8], output: &mut [u8]) {
     use std::arch::x86_64::*;
 
@@ -33,7 +15,25 @@ unsafe fn bit_transpose_avx2_impl(input: &[u8], output: &mut [u8]) {
         unsafe {
             let v0 = _mm256_loadu_si256(in_ptr as *const __m256i);
             let v1 = _mm256_loadu_si256(in_ptr.add(32) as *const __m256i);
-
+            
+            macro_rules! process_bit {
+                ($v0:ident, $v1:ident, $bit_index:literal, $out_ptr:ident) => {
+                    // move target bit into sign bit
+                    let shifted0 = _mm256_slli_epi16($v0, 7 - $bit_index);
+                    let shifted1 = _mm256_slli_epi16($v1, 7 - $bit_index);
+            
+                    // collect sign bits
+                    let mask0 = _mm256_movemask_epi8(shifted0) as u32;
+                    let mask1 = _mm256_movemask_epi8(shifted1) as u32;
+            
+                    // store 16 gathered bits
+                    *($out_ptr as *mut u32) = mask0;
+                    *($out_ptr.add(4) as *mut u32) = mask1;
+            
+                    $out_ptr = $out_ptr.add(8);
+                };
+            }
+            
             process_bit!(v0, v1, 0, out_ptr);
             process_bit!(v0, v1, 1, out_ptr);
             process_bit!(v0, v1, 2, out_ptr);
--- src/algo/bit_transpose/avx512bw.rs
+++ src/algo/bit_transpose/avx512bw.rs
@@ -1,19 +1,4 @@
-﻿macro_rules! process_bit {
-    ($v0:ident, $bit_index:literal, $out_ptr:ident) => {
-        // move target bit into sign bit
-        let shifted0 = _mm512_slli_epi16($v0, 7 - $bit_index);
-
-        // collect sign bits
-        let mask0 = _mm512_movepi8_mask(shifted0) as u64;
-
-        // store 16 gathered bits
-        *($out_ptr as *mut u64) = mask0;
-
-        $out_ptr = $out_ptr.add(8);
-    };
-}
-
-#[target_feature(enable = "avx512bw")]
+﻿#[target_feature(enable = "avx512bw")]
 unsafe fn bit_transpose_avx512bw_impl(input: &[u8], output: &mut [u8]) {
     use std::arch::x86_64::*;
 
@@ -29,7 +14,22 @@ unsafe fn bit_transpose_avx512bw_impl(input: &[u8], output: &mut [u8]) {
     for _ in 0..chunks {
         unsafe {
             let v0 = _mm512_loadu_si512(in_ptr as *const __m512i);
-
+            
+            macro_rules! process_bit {
+                ($v0:ident, $bit_index:literal, $out_ptr:ident) => {
+                    // move target bit into sign bit
+                    let shifted0 = _mm512_slli_epi16($v0, 7 - $bit_index);
+            
+                    // collect sign bits
+                    let mask0 = _mm512_movepi8_mask(shifted0) as u64;
+            
+                    // store 16 gathered bits
+                    *($out_ptr as *mut u64) = mask0;
+            
+                    $out_ptr = $out_ptr.add(8);
+                };
+            }
+            
             process_bit!(v0, 0, out_ptr);
             process_bit!(v0, 1, out_ptr);
             process_bit!(v0, 2, out_ptr);
