ae8478809ccf250e7fbe0765ba92e49e6275582d
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2026-05-25 16:09:35 +0200

    More memory usage output in main simulation.

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,16 @@ fn main() {
 
     let start = std::time::Instant::now();
     let _ = sim.simulate(100_000_000);
+    let end_memory_usage = sim.memory_usage();
     sim.finalize();
     let elapsed = start.elapsed();
 
-    println!("Simulated {} turns in {:?} with {} MiB of memory.", sim.simulated_turns(), elapsed, sim.memory_usage() / 1024 / 1024);
+    let finalized_memory_usage = sim.memory_usage();
+    println!(
+        "Simulated {} turns in {:?} with {} MiB of memory -> {} MiB finalized.",
+        sim.simulated_turns(),
+        elapsed,
+        end_memory_usage / 1024 / 1024,
+        finalized_memory_usage / 1024 / 1024
+    );
 }
