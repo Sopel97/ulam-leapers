7d20459230db0d2c46fe6a74f0c7147ba4f68380
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2026-05-26 19:20:07 +0200

    Start test with egui.

---
+++ src/gui.rs
@@ -0,0 +1,52 @@
+﻿use eframe::egui;
+
+pub fn run() -> eframe::Result<()> {
+    let options = eframe::NativeOptions::default();
+
+    let mut name = "Arthur".to_owned();
+    let mut age = 42;
+    let mut t: f32 = 0.0;
+
+    eframe::run_ui_native("Ulam Leapers Explorer", options, move |ui, _frame| {
+        egui::CentralPanel::default().show_inside(ui, |ui| {
+            let rect = ui.clip_rect(); // Full canvas
+            let painter = ui.painter_at(rect);
+
+            // background
+            painter.rect_filled(rect, 0.0, egui::Color32::BLACK);
+
+            let center = rect.center();
+
+            let radius = rect.width().min(rect.height()) * 0.4;
+
+            let angle = t;
+            t += 0.01;
+
+            let end = egui::pos2(
+                center.x + radius * angle.cos(),
+                center.y + radius * angle.sin(),
+            );
+
+            painter.line_segment(
+                [center, end],
+                egui::Stroke::new(3.0, egui::Color32::WHITE),
+            );
+
+            // Render some controls on top.
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
+            if ui.button("Increment").clicked() {
+                age += 1;
+            }
+            ui.label(format!("Hello '{name}', age {age}"));
+
+            // IMPORTANT: keeps animation running
+            ui.ctx().request_repaint();
+        });
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,37 @@ use ulam_leapers::grid::GridVector;
 use ulam_leapers::piece::LeaperAttacks;
 use ulam_leapers::simulation::{Simulation, SimulationLimits};
 
+const GUI: bool = true;
+
+mod gui;
+
 fn main() {
-    let mut sim = Simulation::new();
-    let p1 = sim.add_player(LeaperAttacks::from_canonical(&GridVector::new(1, 2)));
-    let p2 = sim.add_player(LeaperAttacks::from_canonical(&GridVector::new(1, 2)));
-    sim.add_player_enemy(p1, p2);
-    sim.add_player_enemy(p2, p1);
+    if GUI {
+        gui::run().unwrap(); 
+    } else {
+        let mut sim = Simulation::new();
+        let p1 = sim.add_player(LeaperAttacks::from_canonical(&GridVector::new(1, 2)));
+        let p2 = sim.add_player(LeaperAttacks::from_canonical(&GridVector::new(1, 2)));
+        sim.add_player_enemy(p1, p2);
+        sim.add_player_enemy(p2, p1);
 
-    let start = std::time::Instant::now();
-    let _ = sim.simulate(SimulationLimits::new().with_turn_limit(100_000_000).with_memory_limit(32*1024*1024*1024));
-    let end_memory_usage = sim.memory_usage();
-    sim.finalize();
-    let elapsed = start.elapsed();
+        let start = std::time::Instant::now();
+        let _ = sim.simulate(SimulationLimits::new().with_turn_limit(100_000_000).with_memory_limit(32 * 1024 * 1024 * 1024));
+        let end_memory_usage = sim.memory_usage();
+        sim.finalize();
+        let elapsed = start.elapsed();
 
-    let simulated_turns = sim.simulated_turns();
-    let complete_shells = sim.complete_shells();
-    let frozen_grid = sim.finalize_to_frozen_grid();
-    let finalized_memory_usage = frozen_grid.memory_usage();
-    println!(
-        "Simulated {} turns in {:?}.\nComplete shells: {}.\nEstimated memory usage: {} MiB.\nFinal memory usage: {} MiB.",
-        simulated_turns,
-        elapsed,
-        complete_shells,
-        end_memory_usage / 1024 / 1024,
-        finalized_memory_usage / 1024 / 1024
-    );
+        let simulated_turns = sim.simulated_turns();
+        let complete_shells = sim.complete_shells();
+        let frozen_grid = sim.finalize_to_frozen_grid();
+        let finalized_memory_usage = frozen_grid.memory_usage();
+        println!(
+            "Simulated {} turns in {:?}.\nComplete shells: {}.\nEstimated memory usage: {} MiB.\nFinal memory usage: {} MiB.",
+            simulated_turns,
+            elapsed,
+            complete_shells,
+            end_memory_usage / 1024 / 1024,
+            finalized_memory_usage / 1024 / 1024
+        );
+    }
 }
