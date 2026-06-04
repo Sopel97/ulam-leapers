5735d64ff6d13312eba9d80dbaf1eb414b73c690
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2026-06-04 16:57:13 +0200

    Add minimal docs for trait Subwindow

--- src/gui.rs
+++ src/gui.rs
@@ -17,12 +17,22 @@ pub enum SubwindowResult {
 }
 
 pub trait Subwindow {
+    /// String to be shown as the header
     fn name(&self) -> String;
+    
+    /// This function is called for visible subwindows
     fn ui(self: Box<Self>, ui: &mut Ui) -> SubwindowResult;
+    
+    /// Whether to show the close button
     fn is_closeable(&self) -> bool {
         true
     }
+    
+    /// This function is analogous to ui(), however with the caveat that it's only called
+    /// for background subwindows, i.e. where only the context is available. 
     fn not_ui(self: Box<Self>, ctx: &Context) -> SubwindowResult;
+    
+    /// Called when the subwindow is closed by the close button if allowed by is_closeable.
     fn on_close(&mut self) {}
 }
 
